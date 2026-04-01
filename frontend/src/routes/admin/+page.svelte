f971a7e8d1c03d91c30c72fad6b3ae031b708134
Ryan Emerle <1167333+remerle@users.noreply.github.com>
2026-04-01 17:20:33 -0400

    Replace dropdown menu with icon buttons for edit and delete actions

--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -120,16 +120,13 @@
                 </td>
                 <td><code class="badge badge-ghost font-mono text-xs">{product.sku}</code></td>
                 <td class="font-medium">{formatPrice(product.price)}</td>
-                <td>
-                  <div class="dropdown dropdown-end">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-sm btn-square">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"></path></svg>
-                    </div>
-                    <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-10 w-40 p-2 shadow-lg">
-                      <li><a href="/admin/edit/{product.id}">Edit</a></li>
-                      <li><button class="text-error" onclick={() => openDeleteModal(product)}>Delete</button></li>
-                    </ul>
-                  </div>
+                <td class="whitespace-nowrap">
+                  <a href="/admin/edit/{product.id}" class="btn btn-ghost btn-sm btn-square" title="Edit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
+                  </a>
+                  <button class="btn btn-ghost btn-sm btn-square text-error" title="Delete" onclick={() => openDeleteModal(product)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg>
+                  </button>
                 </td>
               </tr>
             {/each}
